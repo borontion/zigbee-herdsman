dd058d58a4230fc8883a2fafc380151e1dda709c
Pengzhan Zhao <borontion@gmail.com>
2020-05-17 06:24:54 +0800

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+cp ./dist/zcl/definition/cluster.js /opt/zigbee2mqtt/node_modules/zigbee-herdsman/dist/zcl/definition/cluster.js
+cp ./dist/zcl/definition/cluster.js.map /opt/zigbee2mqtt/node_modules/zigbee-herdsman/dist/zcl/definition/cluster.js.map
+cp ./dist/zcl/definition/cluster.d.ts /opt/zigbee2mqtt/node_modules/zigbee-herdsman/dist/zcl/definition/cluster.d.ts
+cp ./dist/zcl/definition/cluster.d.ts.map /opt/zigbee2mqtt/node_modules/zigbee-herdsman/dist/zcl/definition/cluster.d.ts.map
